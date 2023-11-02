1e10e6eaf7fdfc9edf3248388e0dcf816b7b0b48
chathurangak <chathurangak@innosoftwareworld.com>
2023-11-02 15:45:02 +0530

    change to class component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    this.state = {
+      config: null,
+    };
+  }
+
+  componentDidMount() {
+    this.fetchConfig();
+  }
+
+  fetchConfig = async () => {
+    try {
+      const response = await fetch("./config.json");
+      const data = await response.json();
+      this.setState({ config: data });
+    } catch (error) {
+      console.error("Error fetching configuration:", error);
+    }
+  };
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
